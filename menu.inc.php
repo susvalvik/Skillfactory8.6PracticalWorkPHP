5fd089bdd51ff5a4431b70f039b9b5edf12871be
Valentina <66334729+susvalvik@users.noreply.github.com>
2022-11-29 14:56:01 +0300

    Add files via upload

---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul> 
+        <li><a href="#"> Об учебе </a></li> 
+        <li><a href="#"> Портфолио </a></li> 
+        <li><a href="#"> Статьи </a></li> 
+        <li><a href="#"> Ссылки </a></li> 
+        <li><a href="#"> Контакты </a></li> 
+    </ul> 
+</nav>
